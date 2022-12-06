081eb59ffd572ac4a24eb9fc8b920a61f7369f86
Martynas Matimaitis <m.matimaitis@gmail.com>
2022-12-06 13:42:09 +0000

    initial commit

---
+++ api/api_metadata_cache.py
@@ -0,0 +1,28 @@
+api_metadata_cache = {
+    "uk": [
+        {
+            "name": "Third Party A",
+            "url": "https://interview-df854r23.sikoia.com",
+            "endpoint": "/v1/company/{jurisdiction_code}/{company_number}"
+        }
+    ],
+    "nl": [
+        {
+            "name": "Third Party B",
+            "url": "https://interview-df854r23.sikoia.com",
+            "endpoint": "/v1/company-data?jurisdictionCode={jurisdiction_code}&companyNumber={company_number}"
+        }
+    ],
+    "de": [
+        {
+            "name": "Third Party A",
+            "url": "https://interview-df854r23.sikoia.com",
+            "endpoint": "/v1/company/{jurisdiction_code}/{company_number}"
+        },
+        {
+            "name": "Third Party B",
+            "url": "https://interview-df854r23.sikoia.com",
+            "endpoint": "/v1/company-data?jurisdictionCode={jurisdiction_code}&companyNumber={company_number}"
+        }
+    ]
+}
